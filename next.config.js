8336cb4b221f017568e949fdd18f2da2d2e723ad
mrriddwan <83332805+mrriddwan@users.noreply.github.com>
2022-02-28 17:18:46 +0800

    commit

--- next.config.js
+++ next.config.js
@@ -1,7 +1,7 @@
 module.exports = {
     reactStrictMode: true,
     serverRuntimeConfig: {
-        secret: 'THIS IS USED TO SIGN AND VERIFY JWT TOKENS, REPLACE IT WITH YOUR OWN SECRET, IT CAN BE ANY STRING'
+        secret: 'ridwan'
     },
     publicRuntimeConfig: {
         apiUrl: process.env.NODE_ENV === 'development'
